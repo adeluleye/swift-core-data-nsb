a10fce6a2f77444d66db3c44f44c9431c82b2449
adeluleye <adelu4real@gmail.com>
2018-10-07 23:03:47 +0100

    implement tableview functions, register a default tv cell

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var items: [String] = []
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +21,25 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.barTintColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
         navigationController?.navigationBar.tintColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
     @objc func addItem(_ sender: AnyObject) {
         print("added")
     }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item
+        return cell
+    }
 
 
 }
-
