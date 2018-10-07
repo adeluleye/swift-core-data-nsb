4aa5ceebe229e2a385081c671856470666fda8e3
adeluleye <adelu4real@gmail.com>
2018-10-07 23:35:58 +0100

    persist data using CoreData

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
     
-    var items: [String] = []
+    var items: [NSManagedObject] = []
     
     let cellId = "cellId"
 
@@ -25,11 +26,23 @@ class ViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        do {
+            items = try managedContext.fetch(fetchRequest)
+        } catch let err as NSError {
+            print("Failed to fetch items", err)
+        }
+    }
+    
     @objc func addItem(_ sender: AnyObject) {
         let alertController = UIAlertController(title: "Add New Item", message: "Please fill in the textfield below", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
             guard let textField = alertController.textFields?.first, let itemToAdd = textField.text else { return }
-            self.items.append(itemToAdd)
+            self.save(itemToAdd)
             self.tableView.reloadData()
         }
         
@@ -41,6 +54,21 @@ class ViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    func save(_ itemName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(itemName, forKey: "itemName")
+        
+        do {
+            try managedContext.save()
+            items.append(item)
+        } catch let err as NSError {
+            print("Failed to save an item", err)
+        }
+    }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -49,7 +77,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         let item = items[indexPath.row]
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.value(forKeyPath: "itemName") as? String
         return cell
     }
 
